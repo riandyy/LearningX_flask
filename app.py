aca8bcdd1f6d98630d25bff85e67613aa8fba4e4
riandy <riandy20ti@mahasiswa.pcr.ac.id>
2023-01-26 15:31:38 +0700

    freeze

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from http import client
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 
-client = MongoClient('mongodb+srv://riandy:riandyenrisa16@cluster0.66ilm6g.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbmovie
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
